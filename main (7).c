2cf2d056369e5e1fa6f465d69f32418350f65b3f
katik9090 <kartikjethva0@gmail.com>
2025-09-11 17:05:00 +0530

    Add files via upload

---
+++ main (7).c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main(void)
+{
+    /* Replace the name and the number below as needed */
+    printf("Name: Kartik Jethva\n");
+    printf("Chars (no. of characters): 12\n");
+    
+    return 0;
+}
